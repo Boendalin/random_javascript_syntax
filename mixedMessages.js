d2d7cefc13b94d8076af55f6fb773a24bd9ad734
Andreas <125503357+Boendalin@users.noreply.github.com>
2023-05-01 15:34:32 +0200

    Added functions for language, phrase and famous person.

--- mixedMessages.js
+++ mixedMessages.js
@@ -2,5 +2,36 @@ function decideLanguage() {
 
 }
 
-console.log('Hello!')
+function phrase(language) {
+    let phrases = [];
+    switch (language){
+        
+        case "Swedish":
+            phrases = [ "Allt klär en hönsskit.", 
+                            "Var man blir salig på sin tro.", 
+                            "Nä, här blir det inga barn gjorda."];
+            break;
+
+        case "English":
+            phrases = [ "Everything looks good on a chicken turd.",
+                            "Each man will be blessed by his own faith.",
+                            "Nope, no kids will be made here."];
+            break;
+
+        case "Danish":
+            phrases = [ "Allt er smukt på en hønseskidt.",
+                            "Alle vil blive velsignet på sin egen tro.",
+                            "Nej, her kommer da slet ikke blive nogle børn."];
+            break;
+    };
+}
+
+function famousPerson(){
+    let persons = ["Burt Reynolds", "Barrack Obama", 
+                    "Astrid Lindgren", "Mette Fredriksen",
+                    "Abdallah ibn Jakut"]
+
+    return persons[Math.floor(Math.random()*persons.length)]
+}
+console.log(famousPerson())
 
